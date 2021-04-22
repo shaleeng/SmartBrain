cefdf5f94b3838ea6fe524d88b6fecb985b8cc9c
shaleeng <shaleengoel17@gmail.com>
2021-04-21 23:07:43 -0400

    First committ

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import react from 'react'
+import Tilt from 'react-tilt'
+import './Logo.css'
+import brain from './Brain.png'
+
+
+const Logo = () => {
+    return (
+        <div className ='ma4 mt0'>
+            <Tilt className="Tilt br 2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3"> <img style = {{paddingTop: '5px'}}src={brain}/> </div>
+            </Tilt>
+        </div>
+    )
+}
+
+export default Logo;
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import react from 'react'
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Shaleen, your current rank is ...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
